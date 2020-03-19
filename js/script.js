1b1d7ad8416cd4bb8b0e29b3638b87127f84e009
Scott Thomson <scottthomson_57@hotmail.com>
2020-03-19 15:30:34 +1300

    adding heading

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
         play: 5000,
         pagination: false
     })
+
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer"],
+        typeSpeed: 100,
+        startDelay: 1000,
+        showCursor: false
+    })
 })
